8976a2c8d7cafa01fe41a5a0feb7d3242b74d399
Keiichi Kamikawa <k1@a-mix.ne.jp>
2010-04-01 18:47:38 +0900

    dump epg

---
+++ do-epgget.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+CHTYPE=$1
+CHANNEL=$2
+
+TMPTS=$(tempfile).ts
+
+RECPT1=/usr/local/bin/recpt1
+DURATION=60
+
+EPGDUMP=/usr/local/bin/epgdump
+
+$RECPT1 --b25 --strip $CHANNEL $DURATION $TMPTS
+$EPGDUMP $CHTYPE$CHANNEL $CHTYPE $TMPTS -
+
+rm -f $TMPTS
