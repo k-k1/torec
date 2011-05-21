eb2c2139043d9424356901f38ad69b019c6d4a71
Keiichi Kamikawa <k1.kami@gmail.com>
2011-05-22 02:35:54 +0900

    fix bs epgdump

--- do-epgdump.sh
+++ do-epgdump.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+if [ -z $1 ]; then
+  echo "usage: $0 [CHTYPE(GR/BS)] [CHANNEL] [DURATION(default 60sec)]"
+  exit
+fi
+
 CHTYPE=$1
 CHANNEL=$2
-DURATION=${3:=60}
+if [ -z $3 ]; then
+  DURATION=60
+else
+  DURATION=$3
+fi
 
 TMPTS=$(tempfile --suffix=.ts)
 
@@ -10,10 +19,11 @@ RECPT1=/usr/local/bin/recpt1
 EPGDUMP=/usr/local/bin/epgdump
 
 $RECPT1 --b25 --strip $CHANNEL $DURATION $TMPTS 2>/dev/null
-#$RECPT1 --b25 --strip $CHANNEL $DURATION $TMPTS
 if [ $CHTYPE = 'BS' ]; then
   CHANNEL='/BS'
+  $EPGDUMP /$CHTYPE $TMPTS -
+else
+  $EPGDUMP $CHTYPE$CHANNEL $TMPTS -
 fi
-$EPGDUMP $CHANNEL $TMPTS -
 
-#rm -f $TMPTS
+rm -f $TMPTS
