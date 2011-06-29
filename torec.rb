021779114484e75605ef94637e548e1d2471f3e8
Keiichi Kamikawa <k1.kami@gmail.com>
2011-06-29 23:15:17 +0900

    apply code format

--- torec.rb
+++ torec.rb
@@ -119,7 +119,7 @@ class ChannelType < Sequel::Model(:channel_types)
   def tunners
     Tunner.filter(:type => self[:tunner_type]).order(:id).all
   end
-
+  
   def self.types
     ChannelType.all.collect{|r| r[:type]}
   end
@@ -127,8 +127,8 @@ class ChannelType < Sequel::Model(:channel_types)
   def find_empty_tunner(start_time, end_time)
     tunners.each do |t|
       r = Record.exclude(:program_id => nil).
-        eager_graph(:tunner).filter(:tunner__id => t.id).
-        eager_graph(:program).filter{(:program__start_time < start_time) & (:program__end_time > end_time)}
+      eager_graph(:tunner).filter(:tunner__id => t.id).
+      eager_graph(:program).filter{(:program__start_time < start_time) & (:program__end_time > end_time)}
       return t if r.count == 0
     end
     nil
@@ -170,7 +170,7 @@ class Channel < Sequel::Model(:channels)
   def self.find(channel)
     self.filter('type || channel = ?', channel).first
   end
-
+  
   def self.channel_hash
     Hash[*Channel.all.collect{|r| [r.channel_name, r[:id] ]}.flatten]
   end
@@ -250,7 +250,7 @@ class Channel < Sequel::Model(:channels)
     end
     result
   end
-
+  
 end
 
 class Program < Sequel::Model(:programs)
@@ -397,7 +397,7 @@ class Program < Sequel::Model(:programs)
       }
     end
   end
-
+  
   def cancel_reserve
     return if record == nil
     if record.reserve? or record.waiting?
@@ -407,42 +407,42 @@ class Program < Sequel::Model(:programs)
   end
   
   def duration
-    (self[:end_time] - self[:start_time]).to_i
+   (self[:end_time] - self[:start_time]).to_i
   end
   
   def format_duration
     h = (duration / 3600).to_i
     m = ((duration % 3600) / 60).to_i
     s = ((duration % 3600) % 60).to_i
-    (h==0?'':h.to_s+'h') + (m==0?'':m.to_s+'m') + (s==0?'':s.to_s+'s')
+     (h==0?'':h.to_s+'h') + (m==0?'':m.to_s+'m') + (s==0?'':s.to_s+'s')
   end
   
   @@format = Formatter.new({
       :mark => {
-      },
+    },
       :id => {
         :padding => :rjust,
         :length => 6,
-      },
+    },
       :channel => {
         :padding => :ljust,
         :length => 5,
-      },
+    },
       :category => {
         :padding => :ljust,
         :length => 12,
-      },
+    },
       :start_time => {
-      },
+    },
       :duration => {
         :padding => :ljust,
         :length => 8,
-      },
+    },
       :title => {
         :padding => :ljust,
         :length => :adjust,
-      },
-    })
+    },
+  })
   
   def print_line(verbose=false)
     mark = (record==nil)?' ':'*'
@@ -462,25 +462,25 @@ class Program < Sequel::Model(:programs)
     }
     order = [:mark, :id, :channel, :category, :start_time, :duration, :title]
     puts @@format.format(values, order)
-#    print <<-EOF.nopadding
-#      #{mark} #{id.rjust(6)} #{channel.channel_name.ljust(5)} #{category[:type].ljust(12)}
-#       #{start_time} #{duration.ljust(8)} #{self[:title]}
-#    EOF
-#    print <<-EOF.nopadding if verbose
-#      #{channel[:name].rjust(20)} - #{end_time} 
-#      #{self[:description]}
-#    EOF
+    #    print <<-EOF.nopadding
+    #      #{mark} #{id.rjust(6)} #{channel.channel_name.ljust(5)} #{category[:type].ljust(12)}
+    #       #{start_time} #{duration.ljust(8)} #{self[:title]}
+    #    EOF
+    #    print <<-EOF.nopadding if verbose
+    #      #{channel[:name].rjust(20)} - #{end_time} 
+    #      #{self[:description]}
+    #    EOF
   end
   
   def self.now_onair
     now = Time.now
     Program.filter((:start_time <= now) & (:end_time >= now)).order(:channel_id).all
   end
-
+  
   def next
     Program.filter(:start_time > Time.now ).filter(:channel_id => self[:channel_id]).order(:start_time).first
   end
-
+  
   def self.search(opt)
     ds = Program.dataset
     if opt[:channel_id] != nil
@@ -512,7 +512,7 @@ class Program < Sequel::Model(:programs)
   end
   
   def closed?
-    ((self[:end_time] - 60) < Time.now)
+   ((self[:end_time] - 60) < Time.now)
   end
   
   def remaining_second
@@ -655,7 +655,7 @@ class Record < Sequel::Model(:records)
     self[:state] = RESERVE
     save
   end
-
+  
   def cancel
     if reserve?
       self[:job] = nil
@@ -663,7 +663,7 @@ class Record < Sequel::Model(:records)
       save
     end
   end
-
+  
   def schedule
     LOG.info "program_id:#{self[:program_id]} schedule start"
     return if not reserve? and not waiting?
@@ -671,7 +671,7 @@ class Record < Sequel::Model(:records)
       cancel
       return
     end
-
+    
     if waiting?
       #reschedule
       delete_job
@@ -684,7 +684,7 @@ class Record < Sequel::Model(:records)
     if at_start < Time.now
       at_start_str = 'now'
     end
-
+    
     jobid = nil
     start_progs = File.join(APP_DIR,'torec.rb') << " record --start " << program.pk.to_s
     LOG.info "program_id:#{self[:program_id]} schedule at internal commandline : #{start_progs}"
@@ -705,7 +705,7 @@ class Record < Sequel::Model(:records)
     save
     LOG.info "program_id:#{self[:program_id]} scheduled. at jobid=#{self[:job]}"
   end
-
+  
   def stop_recording
     return if not recording?
     begin
@@ -759,7 +759,7 @@ class Record < Sequel::Model(:records)
     LOG.debug "program_id:#{self[:program_id]} recorder args:#{args.join(',')}"
     
     #waiting..
-    (program[:start_time] - 1).wait
+     (program[:start_time] - 1).wait
     rc = find_prev_record
     rc.stop_recording if rc != nil
     
@@ -786,7 +786,7 @@ class Record < Sequel::Model(:records)
     self[:state] = DONE
     save
   end
-
+  
 end
 
 class Torec
@@ -869,132 +869,132 @@ if __FILE__ == $0
   opts = OptionParser.new
   case ARGV.shift
     when 'update'
-      opt = {:file => nil, :channel_id => nil}
-      opts.program_name = $0 + ' update'
-      opts.on("-f", "--file XMLFILE"){|f| opt[:file] = f; Torec.import_from_file(f) }
-      opts.on("-c", "--channel CHANNEL", Channel.channel_hash){|cid| opt[:channel_id] = cid }
-      opts.parse!(ARGV)
-      Torec.update_epg(opt[:channel_id]) if opt[:file] == nil
-      Reservation.update_reserve
+    opt = {:file => nil, :channel_id => nil}
+    opts.program_name = $0 + ' update'
+    opts.on("-f", "--file XMLFILE"){|f| opt[:file] = f; Torec.import_from_file(f) }
+    opts.on("-c", "--channel CHANNEL", Channel.channel_hash){|cid| opt[:channel_id] = cid }
+    opts.parse!(ARGV)
+    Torec.update_epg(opt[:channel_id]) if opt[:file] == nil
+    Reservation.update_reserve
     when 'search'
-      opt = {:channel_id => nil, :category_id => nil, :channel_type => nil, :keyword => nil,
+    opt = {:channel_id => nil, :category_id => nil, :channel_type => nil, :keyword => nil,
         :verbose => false, :reserve => false, :folder => nil, :now => false, :next=> false, :all => false}
-      opts.program_name = $0 + ' search'
-      opts.on("-n", "--now", "display now on-air programs"){opt[:now] = true }
-      opts.on("-N", "--next", "display next on-air programs"){opt[:next] = true }
-      opts.on("-c", "--channel CHANNEL", Channel.channel_hash){|cid| opt[:channel_id] = cid }
-      opts.on("-g", "--category CATEGORY", Category.types_hash){|cid| opt[:category_id] = cid }
-      opts.on("-t", "--type CHANNEL_TYPE", ChannelType.types){|cid| opt[:channel_type] = cid }
-      opts.on("-a", "--all", "display all records."){opt[:all] = true }
-      opts.on("-v", "--verbose", "display program description"){opt[:verbose] = true }
-      opts.on("-r", "--reserve", "add condition to auto-recording"){opt[:reserve] = true }
-      opts.on("-d", "--dir DIRNAME", "auto-recording save directory"){|d| opt[:folder] = d }
-      opts.permute!(ARGV)
-      if opt[:now]
-        Program.now_onair.each do |r|
-          next if opt[:channel_type] != nil and opt[:channel_type] != r.channel[:type]
-          r.print_line(opt[:verbose])
-        end
-        exit
+    opts.program_name = $0 + ' search'
+    opts.on("-n", "--now", "display now on-air programs"){opt[:now] = true }
+    opts.on("-N", "--next", "display next on-air programs"){opt[:next] = true }
+    opts.on("-c", "--channel CHANNEL", Channel.channel_hash){|cid| opt[:channel_id] = cid }
+    opts.on("-g", "--category CATEGORY", Category.types_hash){|cid| opt[:category_id] = cid }
+    opts.on("-t", "--type CHANNEL_TYPE", ChannelType.types){|cid| opt[:channel_type] = cid }
+    opts.on("-a", "--all", "display all records."){opt[:all] = true }
+    opts.on("-v", "--verbose", "display program description"){opt[:verbose] = true }
+    opts.on("-r", "--reserve", "add condition to auto-recording"){opt[:reserve] = true }
+    opts.on("-d", "--dir DIRNAME", "auto-recording save directory"){|d| opt[:folder] = d }
+    opts.permute!(ARGV)
+    if opt[:now]
+      Program.now_onair.each do |r|
+        next if opt[:channel_type] != nil and opt[:channel_type] != r.channel[:type]
+        r.print_line(opt[:verbose])
       end
-      if opt[:next]
-        Program.now_onair.each do |r|
-          next if opt[:channel_type] != nil and opt[:channel_type] != r.channel[:type]
-          r.next.print_line(opt[:verbose])
-        end
-        exit
+      exit
+    end
+    if opt[:next]
+      Program.now_onair.each do |r|
+        next if opt[:channel_type] != nil and opt[:channel_type] != r.channel[:type]
+        r.next.print_line(opt[:verbose])
       end
-      opt[:keyword] = ARGV.join(' ')
-      rsv = Reservation.create(opt)
-      if !rsv.condition?
-        puts opts.help
-        puts "Channels;"
-        Channel.order(:type,:channel).each do |r|
-          puts "   #{r.channel_name.ljust(15)} #{r[:name]}"
-        end
-        puts "Categories;"
-        Category.order(:type).each do |r|
-          puts "   #{r[:type].ljust(15)} #{r[:name]}"
-        end
-        exit
+      exit
+    end
+    opt[:keyword] = ARGV.join(' ')
+    rsv = Reservation.create(opt)
+    if !rsv.condition?
+      puts opts.help
+      puts "Channels;"
+      Channel.order(:type,:channel).each do |r|
+        puts "   #{r.channel_name.ljust(15)} #{r[:name]}"
       end
-      if opt[:reserve]
-        rsv = Reservation.new(rsv.values)
-        rsv.save
-        Reservation.update_reserve
-      else
-        result = Program.search(opt).order(:start_time).all
-        result.each do |r|
-          r.print_line(opt[:verbose])
-        end
+      puts "Categories;"
+      Category.order(:type).each do |r|
+        puts "   #{r[:type].ljust(15)} #{r[:name]}"
       end
+      exit
+    end
+    if opt[:reserve]
+      rsv = Reservation.new(rsv.values)
+      rsv.save
+      Reservation.update_reserve
+    else
+      result = Program.search(opt).order(:start_time).all
+      result.each do |r|
+        r.print_line(opt[:verbose])
+      end
+    end
     when 'reserve'
-      opt = {:reserve_id => nil, :mkdir => false}
-      opts.program_name = $0 + ' reserve'
-      opts.on("--delete RESERVE_ID", Integer, "delete auto-recording condition"){|rid| opt[:reserve_id] = rid }
-      opts.on("--mkdir", "make reserve directories"){ opt[:mkdir] = true }
-      opts.permute!(ARGV)
-      if opt[:mkdir]
-        Reservation.order(:id).each do |r|
-          r.make_output_dir
-        end
-        exit
-      end
-      if opt[:reserve_id] != nil
-        rs = Reservation[opt[:reserve_id]]
-        raise "reservation not found." if rs == nil
-        rs.delete
-        exit
-      end
+    opt = {:reserve_id => nil, :mkdir => false}
+    opts.program_name = $0 + ' reserve'
+    opts.on("--delete RESERVE_ID", Integer, "delete auto-recording condition"){|rid| opt[:reserve_id] = rid }
+    opts.on("--mkdir", "make reserve directories"){ opt[:mkdir] = true }
+    opts.permute!(ARGV)
+    if opt[:mkdir]
       Reservation.order(:id).each do |r|
-        ch = r.channel
-        cate = r.category
-        puts "#{r[:id].to_s.ljust(6)} #{((ch==nil)?'':ch.channel_name).ljust(6)} #{((cate==nil)?'':cate[:type]).ljust(12)} #{r.keyword}"
+        r.make_output_dir
       end
+      exit
+    end
+    if opt[:reserve_id] != nil
+      rs = Reservation[opt[:reserve_id]]
+      raise "reservation not found." if rs == nil
+      rs.delete
+      exit
+    end
+    Reservation.order(:id).each do |r|
+      ch = r.channel
+      cate = r.category
+      puts "#{r[:id].to_s.ljust(6)} #{((ch==nil)?'':ch.channel_name).ljust(6)} #{((cate==nil)?'':cate[:type]).ljust(12)} #{r.keyword}"
+    end
     when 'record'
-      opt = {:program_id => nil, :channel_id => nil, :category_id => nil, :tunner_type => nil, :all => false, :state => nil}
-      opts.program_name = $0 + ' record'
-      opts.on("-c", "--channel CHANNEL", Channel.channel_hash){|cid| opt[:channel_id] = cid }
-      opts.on("-g", "--category CATEGORY", Category.types_hash){|cid| opt[:category_id] = cid }
-      opts.on("-t", "--tunner TUNNER_TYPE"){|type| opt[:tunner_type] = type }
-      opts.on("-a", "--all", "display all records."){opt[:all] = true }
-      opts.on("--schedule [PROGRAM_ID]", "schedule records."){|pid| opt[:state] = :schedule; opt[:program_id] = pid }
-      opts.on("--start PROGRAM_ID"){|pid| opt[:state] = :start; opt[:program_id] = pid }
-      opts.on("--add PROGRAM_ID", Integer, "simple recording"){|pid| opt[:state] = :add; opt[:program_id] = pid }
-      opts.parse!(ARGV)
-      if opt[:program_id] != nil
-        pg = Program[opt[:program_id]]
-        raise "program not found." if pg == nil
-        if opt[:state] == :schedule
-          pg.record.schedule
-        elsif opt[:state] == :start
-          pg.record.start
-        elsif opt[:state] == :add
-          pg.reserve
-        end
-        exit
-      elsif opt[:state] == :schedule
-        opt[:state] = :reserve
-        Record.search(opt).all.each do |rc|
-          rc.schedule
-        end
-        exit
+    opt = {:program_id => nil, :channel_id => nil, :category_id => nil, :tunner_type => nil, :all => false, :state => nil}
+    opts.program_name = $0 + ' record'
+    opts.on("-c", "--channel CHANNEL", Channel.channel_hash){|cid| opt[:channel_id] = cid }
+    opts.on("-g", "--category CATEGORY", Category.types_hash){|cid| opt[:category_id] = cid }
+    opts.on("-t", "--tunner TUNNER_TYPE"){|type| opt[:tunner_type] = type }
+    opts.on("-a", "--all", "display all records."){opt[:all] = true }
+    opts.on("--schedule [PROGRAM_ID]", "schedule records."){|pid| opt[:state] = :schedule; opt[:program_id] = pid }
+    opts.on("--start PROGRAM_ID"){|pid| opt[:state] = :start; opt[:program_id] = pid }
+    opts.on("--add PROGRAM_ID", Integer, "simple recording"){|pid| opt[:state] = :add; opt[:program_id] = pid }
+    opts.parse!(ARGV)
+    if opt[:program_id] != nil
+      pg = Program[opt[:program_id]]
+      raise "program not found." if pg == nil
+      if opt[:state] == :schedule
+        pg.record.schedule
+      elsif opt[:state] == :start
+        pg.record.start
+      elsif opt[:state] == :add
+        pg.reserve
       end
+      exit
+    elsif opt[:state] == :schedule
+      opt[:state] = :reserve
       Record.search(opt).all.each do |rc|
-        r = rc.program
-        print "#{r[:id].to_s.rjust(6)} #{r.channel.channel_name.ljust(5)} "
-        print "#{r.category[:type].ljust(12)} #{r[:start_time].format_display} #{('('+r.format_duration+')').ljust(7)} "
-        print "#{r[:title]}\n"
-        rid = rc[:reservation_id]
-        state = rc[:state].upcase
-        state = state + ' ' + rc[:job].to_s if rc.waiting?
-        puts "   #{(rid==nil)?' ':'A'} #{state.ljust(20)} #{rc[:filename]}"
-        if not rc.reserve? and not rc.waiting?
-          jid = rc[:job]
-          dtime = rc[:done_time]
-          puts "#{' '.ljust(25)} #{rc[:start_time].format_display.ljust(20)}- #{((dtime==nil)?'':dtime.format_display).ljust(20)}"
-        end
+        rc.schedule
       end
+      exit
+    end
+    Record.search(opt).all.each do |rc|
+      r = rc.program
+      print "#{r[:id].to_s.rjust(6)} #{r.channel.channel_name.ljust(5)} "
+      print "#{r.category[:type].ljust(12)} #{r[:start_time].format_display} #{('('+r.format_duration+')').ljust(7)} "
+      print "#{r[:title]}\n"
+      rid = rc[:reservation_id]
+      state = rc[:state].upcase
+      state = state + ' ' + rc[:job].to_s if rc.waiting?
+      puts "   #{(rid==nil)?' ':'A'} #{state.ljust(20)} #{rc[:filename]}"
+      if not rc.reserve? and not rc.waiting?
+        jid = rc[:job]
+        dtime = rc[:done_time]
+        puts "#{' '.ljust(25)} #{rc[:start_time].format_display.ljust(20)}- #{((dtime==nil)?'':dtime.format_display).ljust(20)}"
+      end
+    end
   else
     opts.program_name = $0 + ' COMMAND'
     puts opts.help
